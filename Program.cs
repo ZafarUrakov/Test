a2ed3378bfe92b9078a857e79ac705b9e1b9eda6
ZafarUrakov <goliburakov535@gmail.com>
2023-08-09 11:42:59 -0700

    Triangel Version 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string triangleSize = GetUserValueByMassage("Hello!\nTraingle size: ");
+            double triangleSizeToConvert = Convert.ToDouble(triangleSize);
+            GetUserValueByMassage("\nYour answer is ready, press enter..\n");
+            mechanism(triangleSizeToConvert);
+        }
+        
+        static void mechanism(double worker)
+        {
+            double assistant = worker;
+            
+            for (int leadingSpaces = 1; leadingSpaces <= worker; leadingSpaces++)
+            {
+                for (int secondLeadingSpaces = 0;
+                     secondLeadingSpaces <= worker;
+                     secondLeadingSpaces++)
+                {
+                    if (secondLeadingSpaces >= assistant)
+                        Console.Write("* ");
+                    else
+                        Console.Write(" ");
+                }
+
+                assistant--;
+                Console.WriteLine();
+
+            }
+        }
+
+        static string GetUserValueByMassage(string massage)
+        {
+            Console.Write(massage);
+            return Console.ReadLine();
+        }
+        
+    }
+}
