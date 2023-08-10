e1446f3a00f747dab0ec62a5f1451e1e34bc9519
ZafarUrakov <goliburakov535@gmail.com>
2023-08-09 19:29:17 -0700

    Add other figures.

--- Program.cs
+++ Program.cs
@@ -6,15 +6,25 @@ namespace test
     {
         static void Main(string[] args)
         {
-            string triangleSize = GetUserValueByMassage("Hello!\nTraingle size: ");
-            double triangleSizeToConvert = Convert.ToDouble(triangleSize);
-            GetUserValueByMassage("\nYour answer is ready, press enter..\n");
-            mechanism(triangleSizeToConvert);
+            AssembledSetOfFigures();
         }
         
-        static void mechanism(double worker)
+        static void AssembledSetOfFigures()
         {
-            double assistant = worker;
+            string triangleSize = GetUserValueByMassage("Hello!\nSize: ");
+            int triangleSizeToConvert = Convert.ToInt32(triangleSize);
+            GetUserValueByMassage("\nYour answer is ready, press enter..\n");
+            Engine(triangleSizeToConvert);
+            GetUserValueByMassage("\nPress enter..\n");
+            SecondEngine(triangleSizeToConvert);
+            GetUserValueByMassage("\nDo you want to see the rhombus, press enter..\n");
+            ThirdEngine(triangleSizeToConvert);
+            GetUserValueByMassage("\nUntil we meet again, I was glad to show my skills.\nExit..");
+        }
+        
+        static void Engine(int worker)
+        {
+            int assistant = worker;
             
             for (int leadingSpaces = 1; leadingSpaces <= worker; leadingSpaces++)
             {
@@ -30,9 +40,47 @@ namespace test
 
                 assistant--;
                 Console.WriteLine();
+            }
+        }
+
+        
+        static void SecondEngine(int worker)
+        {
+            for (int leadingSpaces = 0; leadingSpaces < worker; leadingSpaces++)
+            {
+                for (int secondLeadingSpaces = 0;
+                     secondLeadingSpaces <= leadingSpaces;
+                     secondLeadingSpaces++)
+                    Console.Write(" ");
+                
+                for (int thirdLeadingSpaces = 0;
+                     thirdLeadingSpaces <= (worker - leadingSpaces) - 1;
+                     thirdLeadingSpaces++)
+                    Console.Write("* ");
+                Console.WriteLine();
+            }
+        }
+
+        
+        static void ThirdEngine(int worker)
+        {
+            for (int leadingSpaces = 0; leadingSpaces < 2 * worker - 1; leadingSpaces++)
+            {
+                int comparator;
+                if (leadingSpaces < worker)
+                    comparator = 2 * (worker - leadingSpaces) - 1;//2
+                else
+                    comparator = 2 * (leadingSpaces - worker + 1) + 1;//2
+                
+                for(int secondLeadingSpaces = 0; secondLeadingSpaces < comparator; secondLeadingSpaces++)
+                    Console.Write(" ");
+                for(int thirdLeadingSpaces = 0; thirdLeadingSpaces < 2 * worker - comparator; thirdLeadingSpaces++)
+                    Console.Write("* ");
+                Console.WriteLine();
 
             }
         }
+        
 
         static string GetUserValueByMassage(string massage)
         {
